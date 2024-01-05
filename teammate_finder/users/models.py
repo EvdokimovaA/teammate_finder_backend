a119b25fe07091d03133fd82761663acb58e6c59
EvdokimovaA <kykyter2002@gmail.com>
2024-01-05 13:26:34 +0300

    Добавлено отдельное приложение для авторизации/регистрации (пока пустое)

--- teammate_finder/users/models.py
+++ teammate_finder/users/models.py
@@ -4,15 +4,6 @@ from django.contrib.auth.models import AbstractUser
 from datetime import datetime, timedelta, date
 
 
-# та функция проверки не работает
-# def validate_birth_date(input_date):
-#     max_age = 126
-#     current_date = datetime.now().date()
-#     max_diff = timedelta(days=max_age * 365)
-#     if 0 > current_date - input_date > max_diff:
-#         raise ValidationError("Возраст должен быть не больше 126 лет")
-
-
 class Users(AbstractUser):
     GENDER_CHOICES = (
         ('М', 'Мужской'),
@@ -22,7 +13,7 @@ class Users(AbstractUser):
     gender = models.CharField(choices=GENDER_CHOICES, verbose_name='Пол')
     birthday = models.DateField(auto_now=False, verbose_name='Дата рождения', null=True, blank=True, default=date.today)
     who_search = models.TextField(verbose_name='Кого ищу', null=True, blank=True)
-    photo = models.ImageField(upload_to="photos/", null=True, blank=True)
+    photo = models.ImageField(verbose_name='Фото', upload_to="photos/", null=True, blank=True)
     about_me = models.TextField(verbose_name='О себе', null=True, blank=True)
 
     class Meta:
